7e712be8ef4fb86985cbff9b08d7009c3466556a
Ubaid Badar <m.ubaidbadar@gmail.com>
2023-03-27 14:27:12 +0500

    improved-home-designing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <>
             <Header />
-            <div className="max-w-xl mx-auto p-4 grid grid-auto-rows-1fr grid-cols-2 gap-4">
+            <div className="max-w-xl mx-auto lg:p-4 p-3 grid grid-auto-rows-1fr grid-cols-2 md:gap-4 gap-3">
                 <Movie title="The Revenant" src="movie_1.jpg" />
                 <Movie title="Mad Max Fury Road" src="movie_2.jpg" />
                 <Movie title="The Martian" src="movie_4.jpg" />
